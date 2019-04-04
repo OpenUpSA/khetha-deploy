b04e39f2a49adfea70efc1dd06a7cdeadebe4205
Pi Delport <pjdelport@gmail.com>
2019-04-05 00:12:31 +0200

    Add support for building or pulling the base tag

--- deploy-staging.sh
+++ deploy-staging.sh
@@ -1,4 +1,4 @@
 #/bin/sh -ex
 cd "$(dirname "$0")"
 
-./deploy-target.sh staging 'ssh://root@178.128.143.69'
+./deploy-target.sh staging 'ssh://root@178.128.143.69' "$@"
--- deploy-target.sh
+++ deploy-target.sh
@@ -2,7 +2,7 @@
 cd "$(dirname "$0")"
 
 USAGE="
-Usage:  deploy-target NAME DOCKER_HOST
+Usage:  deploy-target NAME DOCKER_HOST [SOURCE_BUILD]
 
 Deploy khetha-django as NAME to DOCKER_HOST.
 
@@ -11,9 +11,13 @@ NAME  (example: 'staging')
 
 DOCKER_HOST  (example: 'ssh://user@host')
     A valid DOCKER_HOST to deploy to.
+
+SOURCE_BUILD (optional, example: 'khetha-django')
+    Path to a source checkout of khetha-django to build.
 "
 TARGET_NAME="${1:?"$USAGE"}"
 TARGET_DOCKER_HOST="${2:?"$USAGE"}"
+SOURCE_BUILD="${3}"
 
 test -z "${DOCKER_HOST}" || { echo "Warning, ignoring existing DOCKER_HOST for local build: ${DOCKER_HOST}"; }
 unset DOCKER_HOST
@@ -26,8 +30,17 @@ set -x
 export BASE_TAG="pidelport/khetha-deploy:${TARGET_NAME}"
 
 
-# Build
-docker build khetha-django --tag "${BASE_TAG}"
+# Build or pull BASE_TAG
+if test -n "${SOURCE_BUILD}"; then
+    echo "Building ${BASE_TAG} from ${SOURCE_BUILD}"
+    docker build "${SOURCE_BUILD}" --tag "${BASE_TAG}"
+else
+    SOURCE_TAG='pidelport/khetha-django:latest'  # XXX: Hard-coded stable source for now.
+    echo "Pulling ${BASE_TAG} from ${SOURCE_TAG}"
+    docker pull "${SOURCE_TAG}"
+    docker tag "${SOURCE_TAG}" "${BASE_TAG}"
+fi
+# Build deployment images from BASE_TAG
 docker-compose build --build-arg BASE_TAG
 
 # Push

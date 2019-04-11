ca46108535bfa66da88a6d4957025f4b4faa1362
Pi Delport <pjdelport@gmail.com>
2019-04-12 01:22:25 +0200

    Pass build args for WhiteNoise

--- deploy-local-test.sh
+++ deploy-local-test.sh
@@ -4,7 +4,10 @@ cd "$(dirname "$0")"
 export BASE_TAG='khetha-local-test:latest'
 
 # Build
-docker build khetha-django --tag "${BASE_TAG}"
+docker build \
+    --build-arg DJANGO_STATICFILES_STORAGE='whitenoise.storage.CompressedManifestStaticFilesStorage' \
+    --build-arg WHITENOISE_KEEP_ONLY_HASHED_FILES='True' \
+    --pull  "${SOURCE_BUILD}" --tag "${BASE_TAG}"
 docker-compose build --build-arg BASE_TAG
 
 docker-compose -f docker-compose.yml -f target.local.yml up
